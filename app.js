9509f5fc6352949da013d9e8101cdfbb2c3e7c09
Faraaz Mohsin <faraazmohsin100@gmail.com>
2019-08-03 02:27:35 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let appId = '138a8b27c66fbcceb4b62dbfadc44c46';
+let units = 'metric';
+let searchMethod = 'q';
+
+function searchWeather(searchTerm) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+    return result.json();
+  }).then(result => {
+    init(result);
+  })
+}
+
+function init(resultFromServer) {
+
+
+
+let cityName = document.getElementById('cityHead');
+let temperatureElem = document.getElementById('temp');
+let unit = document.getElementById('unit');
+
+
+temperatureElem.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+cityName.innerHTML = resultFromServer.name;
+unit.innerHTML = "C";
+
+}
+
+document.getElementById('searchbtn').addEventListener('click', () => {
+  let searchTerm = document.getElementById('searchInput').value;
+  if(searchTerm)
+    searchWeather(searchTerm);
+})
