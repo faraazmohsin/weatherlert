4e7e7c845b21b8eb0b9f82e3554ad654c60fcc37
Faraaz Mohsin <faraazmohsin100@gmail.com>
2019-08-26 19:19:42 -0400

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,11 @@
+const btnscrolltop = document.querySelector("#btnscrolltop");
+
+btnscrolltop.addEventListener("click", function () {
+  //window.scrollTo(0, 0);
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
